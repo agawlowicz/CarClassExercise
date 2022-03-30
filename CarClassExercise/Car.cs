e300ba7b6f277af9a437c19935428d837a6ea5ba
Amanda Gawlowicz <agawlowicz07@gmail.com>
2022-03-30 10:02:44 -0500

    car make, model, and year, practice with classes

---
+++ CarClassExercise/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace CarClassExercise
+{
+    public class Car
+    {
+        public Car()
+        {
+        }
+
+        //Properties
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+
+    }
+}
---
+++ CarClassExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CarClassExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCar = new Car();
+            myCar.Make = "Chrysler";
+            myCar.Model = "Town and Country";
+            myCar.Year = 1999;
+
+            Console.WriteLine($"My Car is a {myCar.Year} {myCar.Make} {myCar.Model}.");
+        }
+    }
+}
